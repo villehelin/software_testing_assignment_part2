f9f7cb325740d44381ff5689383e2eecc0ee09f8
Ville <ville.helin@tuni.fi>
2022-12-06 22:15:31 +0200

    fixed test

--- test/toFinite.test.js
+++ test/toFinite.test.js
@@ -5,7 +5,7 @@ const expect = chai.expect
 
 describe("toFinite", () => {
     it("passing float in toFinite", () =>{
-        expect(toFinite(1/3)).equal(0.33)
+        expect(toFinite(1/3)).to.approximately(0.333, 0.001)
     });
     it("passing 0 in toFinite", () =>{
         expect(toFinite(1/0)).equal(Number.MAX_VALUE)
