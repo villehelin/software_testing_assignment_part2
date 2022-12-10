f933b8882a30705aa3153dced69a7c5e3933d49b
Mikko P <mikko.perala@tuni.fi>
2022-12-10 14:02:00 +0200

    Add: string + int testi

--- test/add.test.js
+++ test/add.test.js
@@ -1,4 +1,4 @@
-import chai from "chai";
+import chai, { Assertion } from "chai";
 import add from "../src/add.js";
 
 const expect = chai.expect
@@ -7,4 +7,9 @@ describe("add", () => {
     it("adds positive number to positive number", () =>{
         expect(add(6,4)).to.equal(10)
     });
+
+    it("adds string to integer number", () => {
+        expect(add("k", 7)).to.equal("k7")
+    });
+
    })
