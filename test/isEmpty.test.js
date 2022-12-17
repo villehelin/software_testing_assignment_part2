b910a055441b64176d8a643a34e943c159709978
Mikko P <mikko.perala@tuni.fi>
2022-12-17 19:34:56 +0200

    Remove commented line, add number to isEmpty test

--- test/isEmpty.test.js
+++ test/isEmpty.test.js
@@ -5,6 +5,7 @@ const expect = chai.expect
 
 describe("isEmpty", () => {
 
+    
     it("passing empty array in isEmpty", () =>{
         const testArray = []
         expect(isEmpty(testArray)).true
@@ -14,40 +15,43 @@ describe("isEmpty", () => {
         const testArray = [1, 2, 3, 5, 8, 13]
         expect(isEmpty(testArray)).false
     });
-
+    
     it("passing null object", () =>{
         expect(isEmpty(null)).true
     });
-
+    
     it("passing empty map", () =>{
         const testMap = new Map();
         expect(isEmpty(testMap)).true
     });
-
+    
     it("passing non-empty map", () =>{
         const testMap = new Map();
         testMap.set('a',1)
         expect(isEmpty(testMap)).false
     });
-
+    
     it("passing object returns false", () =>{
         expect(isEmpty({a: 1, b: 2})).false
     });
-
+    
     it("passing empty object", () =>{
         expect(isEmpty({})).true
     });
-
+    
     it("passing object prototype returns false", () =>{
         function myObject(a,b) {
             this.firstAttr = a;
             this.secondAttr = b;
         }
         myObject.prototype.thirdAttr = "test"
-        // const testPerson = new Person("Barney", 42)
         const testObject = new myObject("A", "B")
 
         expect(isEmpty(testObject.thirdAttr)).false
     });
 
+    it("passing number to isEmpty", () =>{
+        expect(isEmpty(0)).true
+    });
+    
    })
