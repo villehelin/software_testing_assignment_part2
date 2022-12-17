9f105def665950605c39fce6d54d569ba9831f29
Ville <ville.helin@tuni.fi>
2022-12-17 14:01:08 +0200

    tests for filter

--- test/filter.test.js
+++ test/filter.test.js
@@ -10,7 +10,13 @@ describe("filter", () => {
     it("pass object without it being in array to filter", () =>{
         expect(filter({'a': 1, 'b': 1})).to.deep.equal([    []  ])
     });
+    it("pass object and ask for one value that is found in object to filter", () =>{
+        expect(filter([{'a': 1, 'b': 1}, {'a': 3, 'b': 2}], ({ b }) => b == 2)).to.deep.equal([{'a': 3, 'b': 2}])
+    });
+    it("pass object and ask for all value that is found in object to filter", () =>{
+        expect(filter([{'a': 1, 'b': 1}, {'a': 3, 'b': 1}], ({ b }) => b == 1)).to.deep.equal([{'a': 1, 'b': 1}, {'a': 3, 'b': 1}])
+    });
     it("pass object and ask for value that is not found in object to filter", () =>{
-        expect(filter([{'a': 1, 'b': 1}, {'a': 3, 'b': 1}], ({ b }) => 1)).to.deep.equal([{'a': 1, 'b': 1}, {'a': 3, 'b': 1}])
+        expect(filter([{'a': 1, 'b': 1}], ({ b }) => b == 2)).to.deep.equal([    []  ])
     });
    })
